4cc2f1568a4519e27921fdd20256bd55f1378394
johangenis <johan.j.genis@gmail.com>
2019-07-01 21:49:15 +0200

    Modified Dockerfile, settings.py, docker-compose.yml, requirements.txt.

--- app/app/settings.py
+++ app/app/settings.py
@@ -69,14 +69,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "app.wsgi.application"
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    "DEFAULT_PERMISSION_CLASSES": [],
+    "TEST_REQUEST_DEFAULT_FORMAT": "json",
+}
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ.get("DB_HOST"),
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASS"),
     }
 }
 
